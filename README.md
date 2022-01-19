e6e766ced6b526706367df54d3807797d79692c8
takuya-ki <taku8926@gmail.com>
2022-01-20 02:11:14 +0900

    add initial files

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # ur5e_tutorials
-ROS package for Universal Robots UR5e tutorial
+
+ROS package for Universal Robots UR5e tutorial.
+
+## Dependencies
+
+- [ROS Melodic](http://wiki.ros.org/melodic/Installation/Ubuntu)
+- [Universal_Robots_ROS_Driver](https://github.com/UniversalRobots/Universal_Robots_ROS_Driver)
+
+## Installation
+
+1. Install ROS driver  
+
+    $ cd catkin_ws  
+    $ git clone https://github.com/UniversalRobots/Universal_Robots_ROS_Driver.git src/Universal_Robots_ROS_Driver  
+    $ git clone -b calibration_devel https://github.com/fmauch/universal_robot.git src/fmauch_universal_robot  
+    $ rosdep update  
+    $ rosdep install --from-paths src --ignore-src --rosdistro melodic  
+    $ catkin build  
+    $ source ../catkin_ws/devel/setup.bash  
+
+2. Install URCap on a e-series robot by following [here](https://github.com/UniversalRobots/Universal_Robots_ROS_Driver/blob/master/ur_robot_driver/doc/install_urcap_e_series.md)
+    - Don't forget the last step of starting the External Control program on the teach pendant ([known issue](https://github.com/UniversalRobots/Universal_Robots_ROS_Driver/issues/55)).
+    - After [bringup](https://github.com/UniversalRobots/Universal_Robots_ROS_Driver/blob/master/ur_robot_driver/doc/usage_example.md), start the External Control program to establish the connection
+
+## Usage
+
+1. bringup robots  
+    `$ roslaunch ur_robot_driver ur5e_bringup.launch robot_ip:=XX.XX.XX.XX`
+2. launch moveit  
+    `$ roslaunch ur5e_moveit_config ur5e_moveit_planning_execution.launch`
+3. launch rviz  
+    `$ roslaunch ur5e_moveit_config moveit_rviz.launch rviz_config:=$(rospack find ur5e_moveit_config)/launch/moveit.rviz`
+4. execute tutorial demo
+    `$ roslaunch ur5e_tutorials [demo_name].launch`
+
+## Author / Contributor
+
+[Takuya Kiyokawa](https://takuya-ki.github.io/)
+
+## License
+
+This software is released under the MIT License, see [LICENSE](./LICENSE).
---
+++ scripts/wiggle.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+
+import math
+import rospy
+from moveit_commander import RobotCommander, MoveGroupCommander
+
+
+def show_joint_values(mgc):
+    """Shows joint values with rospy log info."""
+    rjs = mgc.get_current_joint_values()
+    rospy.loginfo("Current joint angles.")
+    rospy.loginfo("j1: %f", rjs[0])
+    rospy.loginfo("j2: %f", rjs[1])
+    rospy.loginfo("j3: %f", rjs[2])
+    rospy.loginfo("j4: %f", rjs[3])
+    rospy.loginfo("j5: %f", rjs[4])
+    rospy.loginfo("j6: %f", rjs[5])
+
+
+def go_with_joint_values(mgc, jvs):
+    """Executes the motions with joint values set."""
+    mgc.set_joint_value_target(jvs)
+    mgc.go()
+    rospy.sleep(rospy.Duration.from_sec(1))
+
+
+def wiggle():
+    """Executes wiggle motions."""
+    rospy.init_node("moveit_command_sender", disable_signals=True)
+    robot = RobotCommander()
+    manip = MoveGroupCommander("manipulator")
+
+    # get current status
+    manip_initial_pose = manip.get_current_pose().pose
+    manip_initial_joint_values = manip.get_current_joint_values()
+
+    # set maximum velocity and acceleration
+    manip.set_max_velocity_scaling_factor(0.5)
+    manip.set_max_acceleration_scaling_factor(0.5)
+
+    # generate joint values
+    forward_jvs = [math.radians(90.0),
+                   math.radians(-60.0),
+                   math.radians(-30.0),
+                   math.radians(-30.0),
+                   math.radians(90.0),
+                   math.radians(0.0)]
+    backward_jvs = [math.radians(90.0),
+                    math.radians(-120.0),
+                    math.radians(30.0),
+                    math.radians(30.0),
+                    math.radians(90.0),
+                    math.radians(0.0)]
+    middle_jvs = [math.radians(90.0),
+                  math.radians(-90.0),
+                  math.radians(0.0),
+                  math.radians(0.0),
+                  math.radians(90.0),
+                  math.radians(0.0)]
+
+    # execute wiggle motions two times
+    rospy.loginfo("Start wiggle motions.")
+
+    rospy.loginfo("Moving to middle point...")
+    go_with_joint_values(manip, middle_jvs)
+    show_joint_values(manip)
+    cnt = 0
+    while cnt < 2:
+        rospy.loginfo("Moving forward...")
+        go_with_joint_values(manip, forward_jvs)
+        show_joint_values(manip)
+
+        rospy.loginfo("Moving backward...")
+        go_with_joint_values(manip, backward_jvs)
+        show_joint_values(manip)
+        cnt += 1
+    rospy.loginfo("Moving to middle point...")
+    go_with_joint_values(manip, middle_jvs)
+    show_joint_values(manip)
+
+    # initialize joints
+    rospy.loginfo("Initializing arm joint values...")
+    manip.set_joint_value_target(manip_initial_joint_values)
+    manip.go()
+    rospy.sleep(rospy.Duration.from_sec(1))
+    rospy.signal_shutdown("Finished.")
+
+
+if __name__ == '__main__':
+    try:
+        wiggle()
+    except rospy.ROSInterruptException:
+        pass
