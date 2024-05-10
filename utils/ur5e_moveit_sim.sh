8eeb3542f98b3134422945000775bef0c01b070e
Takuya Kiyokawa <taku8926@gmail.com>
2024-05-10 12:49:24 +0900

    modify the bash commands generated in the docker container

--- utils/ur5e_moveit_sim.sh
+++ utils/ur5e_moveit_sim.sh
@@ -5,6 +5,6 @@ byobu select-pane -t 0
 byobu split-window -v
 byobu select-pane -t 0
 
-byobu send-keys -t 0 'xhost + && docker exec -it ur5e_container bash -it -c "roslaunch ur5e_moveit_config ur5e_moveit_planning_execution.launch demo.launch"' 'C-m'
+byobu send-keys -t 0 'xhost + && docker exec -it ur5e_container bash -it -c "roslaunch ur5e_moveit_config demo.launch"' 'C-m'
 
 byobu attach -t moveit
